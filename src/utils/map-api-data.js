6adeae65cb554d7ca99f8889ded62211cd09dd88
CalinCatuta <calinut123@yahoo.co.uk>
2024-01-19 11:10:49 +0200

    getData acum face fetch si cu Datele despre Remote.

--- src/utils/map-api-data.js
+++ src/utils/map-api-data.js
@@ -1,9 +1,10 @@
 export const mapApiData = (jobs) => {
-    return jobs.map(job => ( { 
-        jobTitle: job.job_title?.[0] ? job.job_title[0] : 'nespecificat', 
-        company: job.company?.[0] ? job.company[0] : 'nespecificat', 
-        location: job.country?.[0] ? job.country[0] : 'nespecificat', 
-        country: job.country?.[0] ? job.country[0] : 'nespecificat', 
-        link: job.job_link?.[0] ? job.job_link[0] : 'nespecificat' 
-    }))
-}
+  return jobs.map((job) => ({
+    jobTitle: job.job_title?.[0] ? job.job_title[0] : 'nespecificat',
+    company: job.company?.[0] ? job.company[0] : 'nespecificat',
+    location: job.country?.[0] ? job.country[0] : 'nespecificat',
+    country: job.country?.[0] ? job.country[0] : 'nespecificat',
+    remote: job.remote?.[0] ? job.remote[0] : 'nespecificat',
+    link: job.job_link?.[0] ? job.job_link[0] : 'nespecificat'
+  }));
+};
